199b0a11bb2bbe8518f48a2bdf3ff6b35ebf5bf7
neii707 <itzur.nei@gmail.com>
2025-11-13 20:02:25 +0700

    Update PROJEK BESAR.py

--- PROJEK BESAR.py
+++ PROJEK BESAR.py
@@ -1,8 +1,8 @@
+
 def menu_admin():
   print()
   print("=== WELCOMEE ADMINN ===")
   
-
 def menu_customer():
   print()
   print("=== WELCOMEE CUSTOMERR ===")
@@ -11,35 +11,8 @@ def menu_produsen():
   print()
   print("=== WELCOMEE PRODUSEN ===  ")
 
-
-#   # print(""" 
-#   /$$$$$$                            /$$ /$$$$$$$            /$$       /$$                        ⠙⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⢹⠿⣿⣿⣿⣿⣿
-#  /$$__  $$                          | $$| $$__  $$          |__/      | $$                        ⣷⣶⡀⠿⠿⣿⣿⣿⣿⣿⣿⡇⠐⠂⢒⡢⠀⣿⣿⣿
-# | $$  \__/  /$$$$$$   /$$$$$$   /$$$$$$$| $$  \ $$  /$$$$$$  /$$  /$$$$$$$  /$$$$$$   /$$$$$$     ⣿⣿⣿⣆⠀⠈⢻⣿⣿⣿⣿⣿⡆⢈⣿⣿⣿⣿⣿⣿
-# |  $$$$$$  /$$__  $$ /$$__  $$ /$$__  $$| $$$$$$$  /$$__  $$| $$ /$$__  $$ /$$__  $$ /$$__  $$    ⣿⣿⣿⣿⣷⣄⠀⠙⠻⢻⢿⣿⠷⢠⢽⣿⣿⣿⣿⣿
-#  \____  $$| $$$$$$$$| $$$$$$$$| $$  | $$| $$__  $$| $$  \__/| $$| $$  | $$| $$  \ $$| $$$$$$$$    ⣿⣿⣿⣿⣿⣿⣧⡀⠁⠀⢘⣱⣍⠿⣾⢿⣿⢿⣿⣿
-#  /$$  \ $$| $$_____/| $$_____/| $$  | $$| $$  \ $$| $$      | $$| $$  | $$| $$  | $$| $$_____/    ⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⢉⢷⣌⠳⣿⣽⣛⣿
-# |  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$$$$$$/| $$      | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠋⠽⠶⡌⣿⣻⣀
-#  \______/  \_______/ \_______/ \_______/|_______/ |__/      |__/ \_______/ \____  $$ \_______/    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣠⡀⠀⠀⠀⠐⠇⢿⣿⣿
-#                                                                            /$$  \ $$              ⠿⠿⠿⠿⠿⠿⠿⠿⠏⠁⠀⠈⠀⠅⠶⠲⠶⠆⠔⠿
-#                                                                           |  $$$$$$/          
-#                                                                            \______/           
-#       """)
-
-# #   print("""
-#  ____                      __  ____                   __                    
-# /\  _`\                   /\ \/\  _`\          __    /\ \                   
-# \ \,\L\_\     __     __   \_\ \ \ \L\ \  _ __ /\_\   \_\ \     __      __   
-#  \/_\__ \   /'__`\ /'__`\ /'_` \ \  _ <'/\`'__\/\ \  /'_` \  /'_ `\  /'__`\ 
-#    /\ \L\ \/\  __//\  __//\ \L\ \ \ \L\ \ \ \/ \ \ \/\ \L\ \/\ \L\ \/\  __/ 
-#    \ `\____\ \____\ \____\ \___,_\ \____/\ \_\  \ \_\ \___,_\ \____ \ \____\ 
-#     \/_____/\/____/\/____/\/__,_ /\/___/  \/_/   \/_/\/__,_ /\/___L\ \/____/
-#                                                                /\____/      
-#                                                                \_/__/             
-     
-#       """)
-
-print(""" 
+def gambar():
+  print(""" 
   █████████                        █████ ███████████             ███      █████                  
  ███░░░░░███                      ░░███ ░░███░░░░░███           ░░░      ░░███                   
 ░███    ░░░   ██████   ██████   ███████  ░███    ░███ ████████  ████   ███████   ███████  ██████ 
@@ -52,17 +25,132 @@ print("""
                                                                                ░░██████          
                                                                                 ░░░░░░                
       """)
+
+def dashboard():
+  try:
+    ds = input("Apakah sudah memiliki akun ? yes/no: ")
+    if ds == "yes":
+      login()
+    elif ds == "no":
+      register()
+  except Exception as e :
+      print(f"Terjadi Error: {e}")
   
+  
+def login():
+  usn = input("Username: ")
+  pw = input("Password: ")
+  if usn == "admin" and pw == "admin":
+    menu_admin()
+  elif usn == "customer" and pw == "customer":
+    menu_customer()
+  elif usn == "produsen" and pw == "produsen":
+    menu_produsen()
+  else:
+    print("PILIHAN TIDAK VALID")
+  print()
+
+def register():
+  while True:
+    try: 
+      print("1. Admin")
+      print("2. Customer")
+      print("3. Produsen")
+      pilih = input("Pilih Akun Role yang ingin anda buat: ")
+      if pilih == "1":
+        data_admin()
+      elif pilih == "2": 
+        data_customer()
+      elif pilih == "3":
+        data_produsen() 
+      break
+    except Exception as e :
+      print(f"Terjadi Error: {e}")
+
+def data_admin():
+  while True:
+    try:
+      usn = input("Masukkan Username: ")
+      if len(usn) > 8 :
+        break
+      else :
+        raise ValueError("Username harus lebih dari 8")
+    except Exception as e :
+      print(f"Terjadi Error: {e}")
+  
+  while True:
+    try:
+      pw = input("Masukkan Password:  ")
+      if len(pw) > 8 :
+        break
+      else :
+        raise ValueError("Password harus lebih dari 8")
+    except Exception as e :
+      print(f"Terjadi Error: {e}")
+  print()
+  print()
+  print("Selamat Akun Anda Telah Dibuat")
+  print()
+  print()
+  gambar()
+  dashboard()
+
+def data_customer():
+  while True:
+    try:
+      usn = input("Masukkan Username: ")
+      if len(usn) > 8 :
+        break
+      else :
+        raise ValueError("Username harus lebih dari 8")
+    except Exception as e :
+      print(f"Terjadi Error: {e}")
+  
+  while True:
+    try:
+      pw = input("Masukkan Password: ")
+      if len(pw) > 8 :
+        break
+      else :
+        raise ValueError("Password harus lebih dari 8")
+    except Exception as e :
+      print(f"Terjadi Error: {e}")
+  print()
+  print()
+  print("Selamat Akun Anda Telah Dibuat")
+  print()
+  print()
+  gambar()
+  dashboard()
+
+def data_produsen():
+  while True:
+    try:
+      usn = input("Masukkan Username: ")
+      if len(usn) > 8 :
+        break
+      else :
+        raise ValueError("Username harus lebih dari 8")
+    except Exception as e :
+      print(f"Terjadi Error: {e}")
+  
+  while True:
+    try:
+      pw = input("Masukkan Password: ")
+      if len(pw) > 8 :
+        break
+      else :
+        raise ValueError("Password harus lebih dari 8")
+    except Exception as e :
+      print(f"Terjadi Error: {e}")
+  print()
+  print()
+  print("Selamat Akun Anda Telah Dibuat")
+  print()
+  print()
+  gambar()
+  dashboard()
+
 print("=== WELCOME TO OUR PLATFROM ===")
-usn = input("Username: ")
-pw = input("Password: ")
-if usn == "admin" and pw == "admin":
-  menu_admin()
-elif usn == "customer" and pw == "customer":
-  menu_customer()
-elif usn == "produsen" and pw == "produsen":
-  menu_produsen()
-else:
-  print("PILIHAN TIDAK VALID")
-print()
-  
+gambar()
+dashboard()
